7a561d87c0bfe254739fdd2a85976278c3225edd
Jack Baron <jackmbaron@gmail.com>
2021-07-14 20:24:51 +0100

    add startup/shutdown logs also disconnect on shutdown

---
+++ src/exit.ts
@@ -0,0 +1,40 @@
+type AsyncExitHook = () => void
+type ExitHook = (exit: AsyncExitHook, error?: Error) => void | Promise<void>
+
+const hooks: Set<ExitHook> = new Set()
+
+export const exitHook = (hook: ExitHook) => {
+  hooks.add(hook)
+}
+
+let terminating = false
+const cleanup = async (error?: Error, code?: number) => {
+  if (terminating === true) return
+  terminating = true
+
+  const jobs = [...hooks].map(
+    async h =>
+      new Promise<void>(resolve => {
+        void h(resolve, error)
+      })
+  )
+
+  await Promise.all(jobs)
+  process.exit(code !== undefined ? code : error !== undefined ? 1 : 0)
+}
+
+export const shutdown = (code?: number) => {
+  void cleanup(undefined, code)
+}
+
+process.on('SIGHUP', async () => cleanup())
+process.on('SIGINT', async () => cleanup())
+process.on('SIGTERM', async () => cleanup())
+process.on('SIGBREAK', async () => cleanup())
+
+process.on('uncaughtException', async error => cleanup(error))
+process.on('unhandledRejection', async error => {
+  await (error instanceof Error
+    ? cleanup(error)
+    : cleanup(new Error('Unknown rejection')))
+})
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
+import { field } from '@lolpants/jogger'
 import { Client } from 'discord.js'
 import sourceMapSupport from 'source-map-support'
 import { TOKEN } from './env/index.js'
+import { exitHook } from './exit.js'
+import { errorField, flush, logger } from './logger.js'
 
 // Enable Source Maps
 sourceMapSupport.install()
 
 const client = new Client({ partials: ['CHANNEL', 'MESSAGE', 'USER'] })
+client.on('ready', async () => {
+  logger.info(
+    field('action', 'ready'),
+    field('user', client.user?.tag ?? 'Unknown')
+  )
+})
+
+exitHook(async (exit, error) => {
+  client.destroy()
+  if (error) {
+    logger.error(errorField(error))
+  } else {
+    logger.info(field('action', 'shutdown'))
+  }
+
+  await flush()
+  exit()
+})
 
 void client.login(TOKEN)
