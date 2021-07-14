fe2bb90069f286d80be1fc6830ade9519e4e08b7
Jack Baron <jackmbaron@gmail.com>
2021-07-14 20:23:43 +0100

    setup discord client

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,3 +5,7 @@ const NODE_ENV = registerString('NODE_ENV')
 const IS_PROD = NODE_ENV?.toLowerCase() === 'production'
 export const IS_DEV = !IS_PROD
 // #endregion
+
+// #region Bot
+export const TOKEN = registerString('TOKEN', true)
+// #endregion
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+import { Client } from 'discord.js'
 import sourceMapSupport from 'source-map-support'
+import { TOKEN } from './env/index.js'
 
 // Enable Source Maps
 sourceMapSupport.install()
+
+const client = new Client({ partials: ['CHANNEL', 'MESSAGE', 'USER'] })
+
+void client.login(TOKEN)
