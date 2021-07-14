61e8f26d4b3ecd4686a794a1d8a8d00322cfbacc
Jack Baron <jackmbaron@gmail.com>
2021-07-14 20:20:44 +0100

    setup logger

---
+++ src/logger.ts
@@ -0,0 +1,33 @@
+import {
+  createConsoleSink,
+  createFileSink,
+  createLogger,
+  field,
+} from '@lolpants/jogger'
+import type { IField } from '@lolpants/jogger'
+import { IS_DEV } from './env/index.js'
+
+const consoleSink = createConsoleSink(IS_DEV)
+const fileSink = createFileSink({
+  name: 'bot',
+  directory: 'logs',
+  debug: IS_DEV,
+})
+
+export const logger = createLogger({
+  name: 'bot',
+  sink: [consoleSink, fileSink],
+})
+
+export const errorField: <T extends Error>(error: T) => Readonly<IField> =
+  error => {
+    const array: Array<Readonly<IField>> = [
+      field('type', error.name),
+      field('message', error.message),
+    ]
+
+    if (error.stack) array.push(field('stack', error.stack))
+    return field('error', array[0], ...array.slice(1))
+  }
+
+export const flush = async () => fileSink.flush()
