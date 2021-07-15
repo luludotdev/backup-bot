fd949cd98670090587435b74ca6c49f16b9e6b44
Jack Baron <jackmbaron@gmail.com>
2021-07-15 07:57:03 +0100

    generate zip for each backup

--- src/backup/index.ts
+++ src/backup/index.ts
@@ -1,7 +1,17 @@
 import type { Guild } from 'discord.js'
+import JSZip from 'jszip'
 import { resolveChannelTree } from './channelTree.js'
 
-export const backupGuild: (guild: Guild) => Promise<void> = async guild => {
+export const backupGuild: (guild: Guild) => Promise<Buffer> = async guild => {
+  const zip = new JSZip()
+
   const tree = await resolveChannelTree(guild)
-  const treeJSON = JSON.stringify(tree, null, 2)
+  zip.file('channel-tree.json', JSON.stringify(tree, null, 2))
+
+  const buffer = await zip.generateAsync({
+    compression: 'DEFLATE',
+    type: 'nodebuffer',
+  })
+
+  return buffer
 }
